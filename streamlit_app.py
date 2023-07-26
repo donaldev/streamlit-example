0a02f5707683936cc72938e08e646786e4d4e342
Donal O'Keeffe <donal@hellopocketed.io>
2023-07-26 17:04:05 +0100

    DIRECT - live API URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def fetch(session, url):
 
 # st.cache
 def fetch_analytics(type,session, start_date, end_date):
-    data = fetch(session,f"""http://localhost:8000/analytics/{type}/?start={start_date}&end={end_date}""")
+    data = fetch(session,f"""https://api.hellopocketed.io/analytics/{type}/?start={start_date}&end={end_date}""")
     print(data)
     return data
 
